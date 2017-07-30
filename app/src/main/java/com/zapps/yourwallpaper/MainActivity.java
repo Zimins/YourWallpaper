1ae8778126eecd42c78a94bb2ec4cf66e8dd2827
지민 <wlals822@naver.com>
2017-07-30 19:07:55 +0900

    load image from gallery

--- app/src/main/java/com/zapps/yourwallpaper/MainActivity.java
+++ app/src/main/java/com/zapps/yourwallpaper/MainActivity.java
@@ -1,7 +1,17 @@
 package com.zapps.yourwallpaper;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,9 +19,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    private static final int REQUEST_LOAD_IMAGE = 100;
+    private static final int REQUEST_READ_STORAGE = 200;
+
+    ImageView backgroundImage;
+    ImageView galleryIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +47,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        backgroundImage = (ImageView) findViewById(R.id.iv_background);
+        galleryIcon = (ImageView) findViewById(R.id.icon_gallery);
+
+        galleryIcon.setOnClickListener(this);
     }
 
     @Override
@@ -85,5 +109,72 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+        switch (viewId) {
+            case R.id.icon_gallery:
+                loadImageFromGallery();
+                break;
+        }
+    }
+
+    private void loadImageFromGallery() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_READ_STORAGE);
+        } else {
+            showImageGallery();
+        }
+    }
+
+    private void showImageGallery() {
+        Intent intent =
+                new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_LOAD_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String picturePath = cursor.getString(columnIndex);
+                cursor.close();
+
+                backgroundImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                backgroundImage.setScaleType(ImageView.ScaleType.MATRIX);
+                backgroundImage.bringToFront();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_READ_STORAGE) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //permission granted
+                showImageGallery();
+            } else {
+                // notify you can't use service
+                Toast.makeText(MainActivity.this, "권한 없이 실행 불가합니다", Toast.LENGTH_SHORT).show();
+                // make notify message
+
+            }
+        }
     }
 }
