b4c75de835e7c26172c028394c0b3c882c2dede4
지민 <wlals822@naver.com>
2017-09-12 11:04:09 +0900

    moved selected image to new activity

--- app/src/main/java/com/zapps/yourwallpaper/activities/MainActivity.java
+++ app/src/main/java/com/zapps/yourwallpaper/activities/MainActivity.java
@@ -1,17 +1,8 @@
 package com.zapps.yourwallpaper.activities;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -105,6 +96,8 @@ public class MainActivity extends AppCompatActivity
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ImageView imageView = drawer.findViewById(R.id.imageView);
+        imageView.setOnClickListener(this);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
@@ -116,74 +109,24 @@ public class MainActivity extends AppCompatActivity
         switch (viewId) {
 
             case R.id.icon_gallery:
-                loadImageFromGallery();
+                Intent intent = new Intent(MainActivity.this, SendImageActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.imageView:
+                Toast.makeText(MainActivity.this, "icon", Toast.LENGTH_SHORT).show();
                 break;
         }
-    }
-
-    private void loadImageFromGallery() {
-        if (ContextCompat
-                .checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    REQUEST_READ_STORAGE);
-
-        } else {
-            showImageGallery();
-        }
-    }
-
-    private void showImageGallery() {
-        Intent intent =
-                new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-        startActivityForResult(intent, REQUEST_LOAD_IMAGE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode != REQUEST_LOAD_IMAGE || resultCode != RESULT_OK) return;
-
-        Uri selectedImage = data.getData();
-        String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-        Cursor cursor = getContentResolver()
-                .query(selectedImage, filePathColumn, null, null, null);
-        cursor.moveToFirst();
-
-        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-        String picturePath = cursor.getString(columnIndex);
-        cursor.close();
-
-        backgroundImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-        backgroundImage.setScaleType(ImageView.ScaleType.MATRIX);
-        backgroundImage.bringToFront();
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-
-        if (requestCode != REQUEST_READ_STORAGE) return;
-
-        if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            //permission granted
-            showImageGallery();
-        } else {
-            // notify you can't use service
-            Toast.makeText(MainActivity.this, "권한 없이 실행 불가합니다", Toast.LENGTH_SHORT).show();
-            // make notify message
-
-        }
-
 
     }
-    // TODO: 2017. 9. 11. upload image to server  
+
+
+    // TODO: 2017. 9. 11. upload image to server
     // TODO: 2017. 9. 11. send messege to partner
 }
--- app/src/main/java/com/zapps/yourwallpaper/activities/SendImageActivity.java
+++ app/src/main/java/com/zapps/yourwallpaper/activities/SendImageActivity.java
@@ -1,15 +1,89 @@
 package com.zapps.yourwallpaper.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.zapps.yourwallpaper.R;
 
 public class SendImageActivity extends AppCompatActivity {
 
+    private static final int REQUEST_READ_STORAGE = 100;
+    private static final int REQUEST_LOAD_IMAGE = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_image);
+        loadImageFromGallery();
+    }
+
+    private void loadImageFromGallery() {
+        if (ContextCompat
+                .checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_STORAGE);
+
+        } else {
+            showImageGallery();
+        }
+    }
+
+    private void showImageGallery() {
+        Intent intent =
+                new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(intent, REQUEST_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode != REQUEST_LOAD_IMAGE || resultCode != RESULT_OK) return;
+
+        Uri selectedImage = data.getData();
+        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+        Cursor cursor = getContentResolver()
+                .query(selectedImage, filePathColumn, null, null, null);
+        cursor.moveToFirst();
+
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String picturePath = cursor.getString(columnIndex);
+        cursor.close();
+
+//        backgroundImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+//        backgroundImage.setScaleType(ImageView.ScaleType.MATRIX);
+//        backgroundImage.bringToFront();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        if (requestCode != REQUEST_READ_STORAGE) return;
+
+        if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //permission granted
+            showImageGallery();
+        } else {
+            // notify you can't use service
+            Toast.makeText(SendImageActivity.this, "권한 없이 실행 불가합니다", Toast.LENGTH_SHORT).show();
+            // make notify message
+
+        }
+
     }
 }
