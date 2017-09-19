99f881fc39a4a8d910a27256d800569065075b41
지민 <wlals822@naver.com>
2017-09-19 20:48:36 +0900

    logout action
    * remove setting by PrefLib
    * move to intro activity

--- app/src/main/java/com/zapps/yourwallpaper/fragments/SettingsFragment.java
+++ app/src/main/java/com/zapps/yourwallpaper/fragments/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.zapps.yourwallpaper.fragments;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.CheckBoxPreference;
@@ -8,8 +9,12 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.zapps.yourwallpaper.Constants;
 import com.zapps.yourwallpaper.R;
+import com.zapps.yourwallpaper.activities.IntroActivity;
+import com.zapps.yourwallpaper.lib.PrefLib;
 
 /**
  * Created by Zimincom on 2017. 8. 3..
@@ -24,7 +29,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_yourwallpaper);
 
-        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        final SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
         PreferenceScreen preferenceScreen = getPreferenceScreen();
 
         int count = preferenceScreen.getPreferenceCount();
@@ -37,6 +42,26 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
                 setPreferenceSummary(preference, value);
             }
         }
+
+        Preference logoutButton = findPreference("key_logout");
+        logoutButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+                // TODO: 2017. 9. 19. remove firebase data and sharedpreference
+                // TODO: 2017. 9. 19. move to first screeen
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+
+                PrefLib.getInstance(getContext()).putBoolean(Constants.KEY_ISCOUPLE, false);
+                PrefLib.getInstance(getContext()).putBoolean(Constants.KEY_ISWAITING, false);
+                Toast.makeText(getContext(), "logout", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext(), IntroActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 
     private void setPreferenceSummary(Preference preference, String value) {
