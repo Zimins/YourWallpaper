ec5901803fda1793347a1f94134dc8c4f17542b1
지민 <wlals822@naver.com>
2017-09-22 14:06:35 +0900

    fixed ui feature
    * button color
    * view adjusts it’s size when show keyboard

--- app/src/main/java/com/zapps/yourwallpaper/activities/MainActivity.java
+++ app/src/main/java/com/zapps/yourwallpaper/activities/MainActivity.java
@@ -41,9 +41,10 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getColor(android.R.color.white));
         setSupportActionBar(toolbar);
 
 
